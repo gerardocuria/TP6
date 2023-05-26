a7eafbd80640d0bf04a4fe7fa4f84e3a655a4d80
Gerardo <gerardocuria193@gmail.com>
2023-05-26 20:31:28 -0300

    TP4 Base

---
+++ build/obj/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.d
@@ -0,0 +1,2 @@
+build/obj/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.o: \
+ /home/gerardo/Facultad/Sistemas_Embebidos/Plantilla_placa/plantilla-embebidos/muju/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.c
